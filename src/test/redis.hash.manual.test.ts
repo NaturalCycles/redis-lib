2ce3d3806b83804e253aab6349095978d2adabc1
mrnagydavid <mrnagydavid@gmail.com>
2024-09-27 12:28:51 +0200

    chore(test): add dao specific tests

--- src/test/redis.hash.manual.test.ts
+++ src/test/redis.hash.manual.test.ts
@@ -1,15 +1,18 @@
+import { CommonKeyValueDao } from '@naturalcycles/db-lib'
 import {
   runCommonHashKeyValueDBTest,
+  runCommonHashKeyValueDaoTest,
   TEST_TABLE,
 } from '@naturalcycles/db-lib/dist/testing'
 import { KeyValueDBTuple } from '@naturalcycles/db-lib/src/kv/commonKeyValueDB'
-import { _AsyncMemo, _range, localTime, pDelay } from '@naturalcycles/js-lib'
+import { _range, localTime, pDelay } from '@naturalcycles/js-lib'
 import { RedisClient } from '../redisClient'
 import { RedisHashKeyValueDB } from '../redisHashKeyValueDB'
 
 const client = new RedisClient()
 const hashKey = 'hashField'
 const db = new RedisHashKeyValueDB({ client, hashKey })
+const dao = new CommonKeyValueDao<Buffer>({ db, table: TEST_TABLE })
 
 afterAll(async () => {
   await client.disconnect()
@@ -20,13 +23,11 @@ test('connect', async () => {
 })
 
 describe('runCommonHashKeyValueDBTest', () => runCommonHashKeyValueDBTest(db))
+describe('runCommonKeyValueDaoTest', () => runCommonHashKeyValueDaoTest(dao))
 
 test('saveBatch with EXAT', async () => {
-  const testIds = _range(1, 4).map((n) => `id${n}`)
-  const testEntries: KeyValueDBTuple[] = testIds.map((id) => [
-    id,
-    Buffer.from(`${id}value`),
-  ])
+  const testIds = _range(1, 4).map(n => `id${n}`)
+  const testEntries: KeyValueDBTuple[] = testIds.map(id => [id, Buffer.from(`${id}value`)])
 
   await db.saveBatch(TEST_TABLE, testEntries, {
     expireAt: localTime.now().plus(1, 'second').unix,
