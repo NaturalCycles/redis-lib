8f26a66aa58366963961df5bad7b719e6d058c4e
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2025-04-05 17:40:44 +0200

    fix: enable verbatimModuleSyntax

--- src/test/redis.hash.manual.test.ts
+++ src/test/redis.hash.manual.test.ts
@@ -1,7 +1,7 @@
 import { CommonKeyValueDao, CommonKeyValueDaoMemoCache } from '@naturalcycles/db-lib'
+import type { KeyValueDBTuple } from '@naturalcycles/db-lib/dist/kv/commonKeyValueDB.js'
 import { runCommonKeyValueDBTest, TEST_TABLE } from '@naturalcycles/db-lib/dist/testing/index.js'
 import { runCommonKeyValueDaoTest } from '@naturalcycles/db-lib/dist/testing/keyValueDaoTest.js'
-import type { KeyValueDBTuple } from '@naturalcycles/db-lib/src/kv/commonKeyValueDB.js'
 import { _AsyncMemo, _range, localTime, pDelay } from '@naturalcycles/js-lib'
 import { afterAll, describe, expect, test } from 'vitest'
 import { RedisClient } from '../redisClient.js'
